f208d19582715fc3d39585dcd5adcc21fc6a8a88
PyuDev <solarwatercodes@gmail.com>
2023-05-31 17:31:26 -0600

    Add print_color function that only supports &str

--- src/lib.rs
+++ src/lib.rs
@@ -1,10 +1,9 @@
-use std::{io, process};
-
-
 pub mod funcs {
     use std::{io::{self, Write}, process};
 
-    /// input: creates a String and then calls io::stdin().read_line to read input for the string
+    use termion::color::{self, Red, Blue, Green, Yellow};
+
+    /// input: creates a String and then calls io::stdin().read_line to read input for the string.
     /// 
     /// s: What to print out before asking for input.
     pub fn input(s: &str)-> String {
@@ -17,14 +16,16 @@ pub mod funcs {
     }
     /// exec: creates a Command with the cmd and arg parameters and returns the output.
     /// 
-    /// cmd: The command to run
+    /// cmd: The command to run.
     /// arg: The command argument.
     pub fn exec(cmd: &str, arg: &str) -> process::Output {
         let output = process::Command::new(cmd).arg(arg).output().expect("Could not run system command");
 
         return output;
     }
-
+    /// number_vec: create a vector with a specific amount of numbers.
+    /// 
+    /// n: the amount of numbers for this vector.
     pub fn number_vec(n:i32) -> Vec<i32> {
         let mut vec: Vec<i32> = Vec::new();
 
@@ -37,8 +38,29 @@ pub mod funcs {
         return vec;
     }
 
+    /// curl: run a curl and print the output to stdout.
+    /// 
+    /// url: the URL to connect to.
     pub fn curl(url:&str) {
         let output = exec("curl", url);
         io::stdout().write_all(&output.stdout).unwrap();
+    }
+
+
+    /// print_color: print text but with color
+    /// 
+    /// s: The string to print out.
+    /// c: The color, either: "red", "blue", "green", or "yellow"
+    pub fn print_color(s: &str, c: &str) {
+        let  col = String::from(c);
+        
+        match col.to_lowercase().trim() {
+            "red" => println!("{}{}", color::Fg(Red), s),
+            "blue" => println!("{}{}", color::Fg(Blue), s),
+            "green" => println!("{}{}", color::Fg(Green), s),
+            "yellow" => println!("{}{}", color::Fg(Yellow), s),
+            _ => println!("Invalid color.")
+        }
+        
     }
 }
