d38a1acdc42298f19f63e04f46a9ce113dee19cf
PyuDev <solarwatercodes@gmail.com>
2023-06-11 16:16:00 -0600

    v0.1.71
    Added play_sound() function.

--- src/lib.rs
+++ src/lib.rs
@@ -1,32 +1,45 @@
-use std::{io::{self, Write}, process::{self, Output}};
+use std::{
+    io::{self, Write, BufReader},
+    fs,
+    thread::sleep,
+    time::Duration,
+    process::{self, Output},
+};
+use rodio::Source;
+use rodio;
 use termion::color;
 use termion::color::*;
 
 /// input: creates a String and then calls io::stdin().read_line to read input for the string.
-/// 
+///
 /// s: What to print out before asking for input.
-pub fn input(s: &str)-> String {
+pub fn input(s: &str) -> String {
     let mut val = String::new();
-    
+
     eprint!("{}", s);
-    io::stdin().read_line(&mut val).expect("Could not read input.");
+    io::stdin()
+        .read_line(&mut val)
+        .expect("Could not read input.");
 
     return val;
 }
 /// exec: creates a Command with the cmd and arg parameters and returns the output.
-/// 
+///
 /// cmd: The command to run.
-/// 
+///
 /// arg: The command argument.
 pub fn exec(cmd: &str, arg: &str) -> process::Output {
-    let output = process::Command::new(cmd).arg(arg).output().expect("Could not run system command");
+    let output = process::Command::new(cmd)
+        .arg(arg)
+        .output()
+        .expect("Could not run system command");
 
     return output;
 }
 /// number_vec: create a vector with a specific amount of numbers.
-/// 
+///
 /// n: the amount of numbers for this vector.
-pub fn number_vec(n:i32) -> Vec<i32> {
+pub fn number_vec(n: i32) -> Vec<i32> {
     let mut vec: Vec<i32> = Vec::new();
 
     for i in 0..n {
@@ -39,40 +52,41 @@ pub fn number_vec(n:i32) -> Vec<i32> {
 }
 
 /// curl: run a curl and print the output to stdout.
-/// 
+///
 /// url: the URL to connect to.
-pub fn curl(url:&str) {
+pub fn curl(url: &str) {
     let output = exec("curl", url);
     io::stdout().write_all(&output.stdout).unwrap();
 }
 
-
 /// change_color: change the console text color
-/// 
+///
 /// c: The color
-/// 
+///
 /// List of colors: ["red", "blue", "green", "yellow", "magenta", "cyan", "white", "black"]
 pub fn change_color(c: &str) {
-    let  col = String::from(c);
-    
+    let col = String::from(c);
+
     match col.to_lowercase().trim() {
         "red" => print!("{}", color::Fg(Red)),
-        "blue" => print!("{}", color::Fg(Blue)), 
+        "blue" => print!("{}", color::Fg(Blue)),
         "green" => print!("{}", color::Fg(Green)),
         "yellow" => print!("{}", color::Fg(Yellow)),
         "magenta" => print!("{}", color::Fg(Magenta)),
-        "cyan" => print!("{}", color::Fg(Cyan)), 
+        "cyan" => print!("{}", color::Fg(Cyan)),
         "white" => print!("{}", color::Fg(White)),
         "black" => print!("{}", color::Fg(Black)),
-        _ => println!("Invalid color.")
+        _ => println!("Invalid color."),
     }
 }
 
 /// output: print out an Output value
-/// 
+///
 /// v: the value to print out
 pub fn output(v: Output) {
-    io::stdout().write_all(&v.stdout).expect("Could not print to stdout");
+    io::stdout()
+        .write_all(&v.stdout)
+        .expect("Could not print to stdout");
 }
 
 /// rainbow_hello: pointless function for a rainbow hello world
@@ -97,4 +111,21 @@ pub fn rainbow_hello() {
     print!("l");
     change_color("magenta");
     print!("d!");
+}
+
+/// play_sound: play a sound with the specified path using the rodio library
+/// 
+/// path: the path for the audio file
+/// 
+/// duration: the amount of time to sleep before the sound ends.
+pub fn play_sound(path: &str, duration:u64) {
+    let stream_handle = rodio::OutputStream::try_default().unwrap();
+
+    let sound_file = BufReader::new(fs::File::open(path).unwrap());
+
+    let source = rodio::Decoder::new(sound_file).unwrap();
+
+    stream_handle.1.play_raw(source.convert_samples()).unwrap();
+
+    sleep(Duration::from_secs(duration));
 }
